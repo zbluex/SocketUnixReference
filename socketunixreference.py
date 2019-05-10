8904514e56e6e37f12c9dbe98d252c483d980e0d
z00347042 <zhuqi1@huawei.com>
2019-05-10 14:21:31 +0800

    init

---
+++ socketunixreference.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import commands
+import time
+import os
+import re
+import argparse
+import sys
+
+
+def logerror(handler, errcode):
+	if errcode > 0:
+		print("[ERROR]%s failed, Error Code: %s\n") % (handler, os.strerror(errcode))
+		exit(1)
+
+
+class IPCRef(object):
+	# define CONST
+	__netstatTcpUdpCmd = "netstat -atunp|grep -v '-'"
+	__netstatUnixCmd = "netstat -axnp"
+	__unixInfoCmd = "ss -xp|grep -v Netid"
+	#define variable
+	tcpUdpListenMap = dict()
+	tcpUdpEstablishedListData = list()
+	unixListenMap = dict()
+	unixPathMap = dict()
+	unixPearPortMap = dict()
+
+	def calculate_socket_reference(self):
+		self.get_tcpudp_info()
+		for l in self.tcpUdpEstablishedListData:
+					if self.tcpUdpListenMap.has_key(l[4]):
+							if  self.tcpUdpListenMap[l[4]][1].has_key(l[-1]):
+								self.tcpUdpListenMap[l[4]][1][l[-1]] = self.tcpUdpListenMap[l[4]][1][l[-1]] + 1
+							else:
+								self.tcpUdpListenMap[l[4]][1][l[-1]] = 1
+
+	def get_tcpudp_info(self):
+		self.tcpUdpEstablishedListData = list()
+
+		netinfo = commands.getstatusoutput(self.__netstatTcpUdpCmd)
+		logerror("Get socket information", netinfo[0])
+		netinfo = netinfo[1].split('\n')
+		listenList = list()
+		establishedList = list()
+		for l in netinfo:
+			if l.find('LISTEN') >= 0 or l.find('0.0.0.0:*') >= 0 or l.find(':::*') >= 0:
+				listenList.append(l)
+			else:
+				establishedList.append(l)
+
+		for l in listenList:
+			info = l.split()
+			if not self.tcpUdpListenMap.has_key(info[3]):
+				self.tcpUdpListenMap[info[3]] = list()
+				self.tcpUdpListenMap[info[3]].append(info[-1])
+				self.tcpUdpListenMap[info[3]].append(dict())
+
+		for l in establishedList:
+			self.tcpUdpEstablishedListData.append(l.split())
+
+	def get_unix_info(self):
+		self.unixPathMap.clear()
+		self.unixPearPortMap.clear()
+
+		netinfo = commands.getstatusoutput(self.__netstatUnixCmd)
+		logerror("Get socket information", netinfo[0])
+		netinfo = netinfo[1].split('\n')
+		for l in netinfo:
+			e = l.split()
+			if l.find('LISTEN') >= 0 and e[-2] != '-' :
+				if not self.unixListenMap.has_key(e[-1]):
+					self.unixListenMap[e[-1]] = list()
+					self.unixListenMap[e[-1]].append(e[-2])
+					self.unixListenMap[e[-1]].append(dict())
+		netinfo = commands.getstatusoutput(self.__unixInfoCmd)
+		logerror("Get unix information", netinfo[0])
+		netinfo = netinfo[1].split('\n')
+		unix_info_list = list()
+		for l in netinfo:
+			e = l.split()
+			if e[4] != '*':
+				if not self.unixPathMap.has_key(e[4]):
+					self.unixPathMap[e[4]] = list()
+				self.unixPathMap[e[4]].append(e)
+			self.unixPearPortMap[e[5]] = e
+
+	def calculate_unix_reference(self):
+		self.get_unix_info()
+		for k in self.unixListenMap.keys():
+			if self.unixPathMap.has_key(k):
+				for l in self.unixPathMap[k]:
+					if self.unixPearPortMap.has_key(l[7]):
+						name = re.split('\(\(|\)\)', l[-1])[1]
+						name = re.split('"', name)[1:]
+						key = re.split('=|,',name[1])[2] + "/" + name[0]
+						if self.unixListenMap[k][1].has_key(key):
+							self.unixListenMap[k][1][key] = self.unixListenMap[k][1][key] + 1
+						else:
+							self.unixListenMap[k][1][key] = 1
+
+	def calculate_reference(self):
+		self.calculate_socket_reference()
+		self.calculate_unix_reference()
+
+
+def get_parser():
+	# parse args
+	parser = argparse.ArgumentParser(
+		description="Socket and Unix Reference analyze tool;",
+		epilog="author: z00347042 zhuqi1@huawei.com",
+		version="1.0.0"
+	)
+	parser.add_argument("-t", "--time", dest="time",
+						help="analyze consistence time, second",
+						default="1",
+						metavar="int")
+
+	return parser
+
+
+def args_handler(args):
+	global timeout
+	if int(args.time) > 0:
+		timeout = int(args.time)
+
+	return
+
+
+if __name__ == "__main__":
+	timeout = 1
+
+	g_parser = get_parser()
+	args_handler(g_parser.parse_args())
+
+	ipc_ref = IPCRef()
+	while timeout > 0:
+		ipc_ref.calculate_reference()
+		timeout = timeout - 1
+		if timeout > 0:
+			time.sleep(1)
+
+	print("Socket Reference:")
+	for k,v in ipc_ref.tcpUdpListenMap.items():
+		print k,v
+
+	print("")
+	print("Unix Reference:")
+	for k,v in ipc_ref.unixListenMap.items():
+		print k,v
+
